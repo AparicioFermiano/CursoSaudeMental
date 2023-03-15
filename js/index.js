40cef37bbf1f254079e7d76869c3fa65ff7e1303
AparicioFermiano <aparicio.empresarial@gmail.com>
2023-03-14 22:02:21 -0300

    Mais slides prontos, trabalhando no popover

--- js/index.js
+++ js/index.js
@@ -22,4 +22,42 @@ function mudar_carrosel(id, sem_nav) {
 
 	$('html, body').scrollTop(0);
   }
-	
+
+
+  const btn = document.getElementById("btn");
+  const popover = document.getElementById("popover");
+  
+  btn.addEventListener("click", function() {
+	popover.style.display = "block";
+	const btnRect = btn.getBoundingClientRect();
+	const popoverRect = popover.getBoundingClientRect();
+	const top = btnRect.top - popoverRect.height - 10;
+	const left = btnRect.left + (btnRect.width - popoverRect.width) / 2;
+	const right = btnRect.left + btnRect.width + 10;
+	const bottom = btnRect.top + btnRect.height + 10;
+	const windowWidth = window.innerWidth;
+	const windowHeight = window.innerHeight;
+	let positionClass = "popover-top";
+	if (top > 0) {
+	  positionClass = "popover-top";
+	} else if (bottom + popoverRect.height < windowHeight) {
+	  positionClass = "popover-bottom";
+	} else if (left > 0) {
+	  positionClass = "popover-left";
+	} else if (right + popoverRect.width < windowWidth) {
+	  positionClass = "popover-right";
+	}
+	popover.classList.add(positionClass);
+  });
+  
+  popover.addEventListener("click", function() {
+	popover.style.display = "none";
+	popover.className = "";
+  });
+
+  document.addEventListener("click", function(event) {
+	if (event.target !== btn && !popover.contains(event.target)) {
+	  popover.style.display = "none"; // ou popover.parentNode.removeChild(popover);
+	}
+  });
+  
