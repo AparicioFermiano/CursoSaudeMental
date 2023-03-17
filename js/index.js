d25e252baa52496b3307f86de9489a30d5d03af4
AparicioFermiano <aparicio.empresarial@gmail.com>
2023-03-17 00:36:30 -0300

    Todos slides feitos

--- js/index.js
+++ js/index.js
@@ -12,32 +12,19 @@ function nav() {
 }
 
 function mudar_carrosel(id, sem_nav) {
-    $('#carousel').carousel($(id).index());
-    $('.sidebar-item').removeClass('active');
-    $('#list-' + id.substring(1)).addClass('active');
+    const id_pai = $(id.split('-')[0])
+    const id_lista = '#list-' + id.substring(1);
 
+    $('#carousel').carousel($(id).index());
+    $('.sidebar-item, .sidebar-subitem').removeClass('active');
+    $(id_lista).addClass('active');
+
+    if (id_pai.length && id_pai.find('.active').length) {
+        id_pai.addClass('active');
+    }
     if (sem_nav !== 1) {
         nav();
     }
 
     $('html, body').scrollTop(0);
 }
-
-// Obter as dimensões do vídeo
-var video = new Image();
-video.src = "url-do-video";
-video.onload = function() {
-    var width = this.naturalWidth;
-    var height = this.naturalHeight;
-
-    // Ajustar o tamanho do iframe
-    var iframe = document.getElementById("video-iframe");
-    iframe.setAttribute("width", width + "px");
-    iframe.setAttribute("height", height + "px");
-
-    // Adicionar um ouvinte de evento resize
-    window.addEventListener("resize", function() {
-        iframe.setAttribute("width", width + "px");
-        iframe.setAttribute("height", height + "px");
-    });
-};
